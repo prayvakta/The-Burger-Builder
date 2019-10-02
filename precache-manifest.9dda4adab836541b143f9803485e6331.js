62a9e609f0633b4ed6d64f44fa8c92edfef9cc13
pavakta <pavakta@myseneca.ca>
2019-10-02 10:36:57 -0400

    Updates

---
+++ precache-manifest.9dda4adab836541b143f9803485e6331.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30dc8ba70d6a59c588187cb87d23e077",
+    "url": "/The-Burger-Builder/index.html"
+  },
+  {
+    "revision": "3305dd471fb3caede005",
+    "url": "/The-Burger-Builder/static/css/main.ab25f562.chunk.css"
+  },
+  {
+    "revision": "133a9604cf885e95f8c4",
+    "url": "/The-Burger-Builder/static/js/2.51c58efa.chunk.js"
+  },
+  {
+    "revision": "3305dd471fb3caede005",
+    "url": "/The-Burger-Builder/static/js/main.111b4be1.chunk.js"
+  },
+  {
+    "revision": "be963a1e413fa7080520",
+    "url": "/The-Burger-Builder/static/js/runtime~main.d5495f80.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/The-Burger-Builder/static/media/burger-logo.b8503d26.png"
+  }
+]);
